5c708fc449f3914b7f4f5a3a7c58bf902175ccb1
liphenste <liphenste@gmail.com>
2019-05-27 22:59:40 -0400

    add basic information

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 
 function Header(props) {
   return (
-    <div id="header"></div>
+    <div id="header">
+      <h1 id="name">stephen li.</h1>
+      <p className="tagline" id="tagline1">software developer.</p>
+      <p className="tagline" id="tagline2">uwaterloo cs '22.</p>
+      <p id="greeting">welcome to my home.</p>
+    </div>
   );
 }
 
 function About(props) {
   return (
     <div id="about">
-      <div className="description"></div>
+      <p className="description">
+      Stephen Li is studying Computer Science at the University of Waterloo. Inquisitive at heart, he is captivated by our evolving technological era that speaks the language of computers.
+      He is constantly on the search for new solutions, in hopes of making his own contribution to the world.
+      When not on his coding assignments, you can find Stephen problem-solving through his online chess/poker games and enjoying aesthetic pictures of cats.
+      </p>
     </div>
   );
 }
@@ -19,24 +28,26 @@ function About(props) {
 function Project(props) {
   return (
     <div className="project">
-      <div className="title"></div>
-      <div className="description"></div>
+      <h2 className="title">{props.title}</h2>
+      <p className="description">{props.description}</p>
     </div>
   );
 }
 
 function Projects(props) {
+  const cliChessDescription = "a lightweight command-line based chess interface for personal use"
+  const bpmCalcDescription = "angularjs widget that calculates bpm based on rate of user input"
   return (
     <div id="projects">
-      <Project />
-      <Project />
+      <Project title="cli_chess" description={cliChessDescription} />
+      <Project title="bpm-calculator" description={bpmCalcDescription} />
     </div>
   );
 }
 
 function Contact(props) {
   return (
-    <div id="contact"></div>
+    <p id="contact">find me at <a href="mailto:liphenste@gmail.com">liphenste@gmail.com</a>.</p>
   );
 }
 
